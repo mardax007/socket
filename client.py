ffcf2fb4cdaec4aeb269a025635de07e6626386f
mardax007 <62964422+mardax007@users.noreply.github.com>
2020-07-07 21:04:22 +0200

    edit

--- client.py
+++ client.py
@@ -8,24 +8,25 @@ DISCONNECT_MESSAGE = "!DISCONNECT"
 SERVER = "192.168.56.1"
 ADDR = (SERVER, PORT)
 
-messagefromuser = ""
-
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
 def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
-    print(client.recv(2048).decode(FORMAT))
+    if msg != "" or msg != " ":
+        message = msg.encode(FORMAT)
+        msg_length = len(message)
+        send_length = str(msg_length).encode(FORMAT)
+        send_length += b' ' * (HEADER - len(send_length))
+        client.send(send_length)
+        client.send(message)
+        print(client.recv(2048).decode(FORMAT))
+        print("Message: %s" % (e1.get()))
+    else:
+        print("NAM")
 
 def show_entry_fields():
     send(e1.get())
-    print("Message: %s" % (e1.get()))
-    messagefromuser = e1.get()
+    e1.update()
 
 def exitapp():
     send(DISCONNECT_MESSAGE)
@@ -39,7 +40,6 @@ while True:
     e1 = tk.Entry(master)
 
     e1.grid(row=0, column=1)
-
     tk.Button(master, text='Quit', command=exitapp).grid(row=3, column=0, sticky=tk.W, pady=4)
     tk.Button(master, text='Send', command=show_entry_fields).grid(row=3, column=1, sticky=tk.W, pady=4)
 
