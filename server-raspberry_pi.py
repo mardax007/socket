fe6b1bb0d32429ae0c831f43a60dadb0a5123314
mardax007 <62964422+mardax007@users.noreply.github.com>
2020-07-11 14:50:10 +0200

    added rpi support

---
+++ server-raspberry_pi.py
@@ -0,0 +1,50 @@
+import socket
+import threading
+from datetime import datetime
+
+HEADER = 64
+PORT = 57952
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT-0YXf$j0daPn7&^PyQ8ups&dSF4HsVASCNPReNrd%rjQ^K#JII$4yagqnw0vwReX!r3!cY3QICiM%OEUMfcZA95ulH9*km0%nr6w"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    dateandtime = datetime.now()
+    print("[NEW CONNECTION] " + addr + " connected.")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                conn.send("DISCONNECTED".encode(FORMAT))
+                print("Bye bye " + addr)
+                connected = False
+
+            else:
+                dateandtime = datetime.now()
+                print("[" + addr + "] " + msg)
+                conn.send("Msg received".encode(FORMAT))
+
+    conn.close()
+
+
+def start():
+    server.listen()
+    dateandtime = datetime.now()
+    print("[LISTENING] Server is listening on " + SERVER)
+    while True:
+        connections = threading.activeCount() - 1
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print("[ACTIVE CONNECTIONS] " + connections)
+dateandtime = datetime.now()
+print("[STARTING] server is starting...")
+start()
